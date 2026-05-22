44408a4c9e7d3e263dbba78425cfed8bc93ed9e2
phowell13 <phowell085@gmail.com>
2026-05-22 16:17:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from pytrends.request import TrendReq
-
 # =====================================================
 # GOOGLE TRENDS SETUP
 # =====================================================
